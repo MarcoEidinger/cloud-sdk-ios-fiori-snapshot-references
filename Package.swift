da922d396f77d8a30c94129ff6e40e0af4d54fc0
Marco Eidinger <marco.eidinger@sap.com>
2020-07-16 15:07:19 -0700

    fake package

--- Package.swift
+++ Package.swift
@@ -6,12 +6,22 @@ import PackageDescription
 let package = Package(
     name: "cloud-sdk-ios-fiori-snapshot-references",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "cloud-sdk-ios-fiori-snapshot-references",
+            type: .dynamic,
+            targets: ["cloud-sdk-ios-fiori-snapshot-references"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
+        .target(
+            name: "cloud-sdk-ios-fiori-snapshot-references",
+            dependencies: [],
+            resources: [
+            .copy("FioriCharts/LineChartSnapshotTests/testLineChartPositiveValuesSingleSeries-iPadPro11_landscape_light.1.png")]
+        ),
     ]
 )
