19f9f77a4c58e2932ed4b864b449d9818c4ddd43
Marco Eidinger <marco.eidinger@sap.com>
2020-07-16 12:55:23 -0700

    swift package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "cloud-sdk-ios-fiori-snapshot-references",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+    ]
+)
